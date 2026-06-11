35d9b64377e5d8fb4cd94e619e69256daf102c24
Giriraj <jgirirajkumar@gmail.com>
2026-06-11 22:16:19 +0530

    Solved recursion factorial question

---
+++ Easy/RecursionFactorial.java
@@ -0,0 +1,16 @@
+package Easy;
+
+class RecursionFactorial {
+
+    public static long Factorial(int n){
+        if(n <= 0 || n == 1){
+            return 1;
+        }
+        return n*Factorial(n - 1);
+    }
+
+    public static void main(String[] args){
+      long result = Factorial(5);
+      System.out.println(result);
+    }
+}
