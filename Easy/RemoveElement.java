c2eca307867e47b7726b0d49f9c1ccce5609f5cb
Giriraj <jgirirajkumar@gmail.com>
2026-06-14 11:57:28 +0530

    Solved Remove element question

---
+++ Easy/RemoveElement.java
@@ -0,0 +1,21 @@
+package Easy;
+
+class RemoveElement{
+    public static int Element(int[] arr , int val , int val2){
+        int k = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != val && arr[i] != val2){
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,4,4,5};
+        int val = 4;
+        int val2 = 2;
+        int result = Element(arr,val,val2);
+        System.out.println(result);
+    }
+}
