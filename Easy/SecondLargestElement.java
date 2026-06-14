92c3b508799edc16452db545c0e40e684968d768
Giriraj <jgirirajkumar@gmail.com>
2026-06-14 13:47:43 +0530

    Solved Second largest element problem in java

---
+++ Easy/SecondLargestElement.java
@@ -0,0 +1,22 @@
+package Easy;
+
+class SecondLargestNumber{
+    public static int Printer(int[] num){
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for(int nums : num){
+            if(nums > largest){
+                secondLargest = largest;
+                largest = nums;
+            }else if(nums > secondLargest && nums != largest){
+                secondLargest = nums;
+            }
+        }  
+        return secondLargest;
+    }
+    public static void main(String[] args){
+        int[] num = {10, 5, 20, 8, 20};
+        int result = Printer(num);
+        System.out.print(result);
+    }
+}
---
+++ Easy/ShiftZerosInArray.java
@@ -0,0 +1,23 @@
+package Easy;
+
+class ShiftZerosInArray{
+    public static void Printer(int[] arr){
+        int k = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] != 0){
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+            for(int i = k; i<arr.length; i++){
+                arr[i] = 0;
+            }
+    }
+    public static void main(String[] args){
+        int[] arr = {0,1,0,2,3,4};
+        Printer(arr);
+        for(int nums : arr){
+            System.out.println(nums + " ");
+        }
+    }
+}
