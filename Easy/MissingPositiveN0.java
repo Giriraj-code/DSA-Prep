f3b98068a85f790bfc13ef7106530825ed7e28f7
Giriraj <jgirirajkumar@gmail.com>
2026-06-22 22:17:43 +0530

    Solved Missing positive number question

---
+++ Easy/MissingPositiveN0.java
@@ -0,0 +1,28 @@
+package Easy;
+import java.util.HashSet;
+import java.util.Set;
+public class MissingPositiveN0 {
+    private static int missingNumber(int arr[])
+    {
+        Set<Integer> set = new HashSet<>();
+        
+        for (int num : arr) {
+            if (num > 0) {
+                set.add(num);
+            }
+        }
+        
+        
+        int smallest = 1;
+        while (set.contains(smallest)) {
+            smallest++;
+        }
+        
+        return smallest;
+    }
+    public static void main(String[] args) {
+        int []arr=new int[]{2, -3, 4, 1, 1, 7};
+        System.out.println(missingNumber(arr));
+    }
+    
+}//Output: 3
