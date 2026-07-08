529ae9fa2876047b399ed744e760eaa7442b6358
Giriraj <jgirirajkumar@gmail.com>
2026-07-08 20:37:59 +0530

    understand hashmap in java

---
+++ Easy/Myhashmappractise.java
@@ -0,0 +1,12 @@
+package Easy;
+import java.util.HashMap;
+public class Myhashmappractise {
+    public static void main(String[] args){
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("Rahul",01);
+        map.put("Rohit",02);
+        map.put("Ramesh",03);
+
+        System.out.println(map.get("Rahul"));
+    }
+}
