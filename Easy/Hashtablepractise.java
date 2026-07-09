3e9bc8744404ebd7d2634c4d4471cb212044dc4b
Giriraj <jgirirajkumar@gmail.com>
2026-07-09 22:40:24 +0530

    initial commit

---
+++ Easy/Hashtablepractise.java
@@ -0,0 +1,20 @@
+package Easy;
+import java.util.Hashtable;
+class Hashtablepractise{
+    public static void main(String[] args){
+        Hashtable<String, Integer> ht = new Hashtable<>();
+
+        ht.put("Ramos", 01);
+        ht.put("Vamos", 02);
+        ht.put("Momos", 03);
+
+        System.out.println("Hash table size " + ht.size());
+        System.out.println(ht);
+
+
+        if(ht.containsKey("Vamos")){
+            Integer a = ht.get("Vamos");
+            System.out.println(a);
+        }
+    }
+}
