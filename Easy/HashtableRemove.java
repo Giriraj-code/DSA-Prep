dcc1bed4bd162f93249c79762bd6932d3087a1bd
Giriraj <jgirirajkumar@gmail.com>
2026-07-09 23:34:57 +0530

    Hashtable remove element

---
+++ Easy/HashtableRemove.java
@@ -0,0 +1,21 @@
+package Easy;
+import java.util.Hashtable;
+class HashtableRemove{
+
+    public static void main(String[] args){
+        Hashtable<Integer , String> ht = new Hashtable<Integer , String>();
+
+        ht.put(01,"Boxer");
+        ht.put(02,"Runner");
+        ht.put(03,"Swimmer");
+        ht.put(04,"Dancer");
+
+        System.out.println("Default Hashtable "+ht);
+
+        ht.remove(01);
+
+        System.out.println("After removing element in Hashtable "+ht);
+
+    }
+
+}
