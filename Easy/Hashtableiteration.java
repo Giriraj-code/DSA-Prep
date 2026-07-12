df22115bea1f7919169c8dbb34e673573f777e6e
Giriraj <jgirirajkumar@gmail.com>
2026-07-12 21:30:23 +0530

    Initial commit

--- Easy/Hashtableiteration.java
+++ Easy/Hashtableiteration.java
@@ -9,11 +9,11 @@ class Hashtableiteration{
 
         Hashtable<String,Integer> ht = new Hashtable<>();
 
-        ht.put("Raman",01);
-        ht.put("Shyam",02);
-        ht.put("Rahul",03);
-        ht.put("Gopal",04);
-        ht.put("Shivu",05);
+        ht.put("Raju",01);
+        ht.put("Vishnu",02);
+        ht.put("Simon",03);
+        ht.put("Golu",04);
+        ht.put("Shiv",05);
 
         for(Map.Entry<String,Integer> e : ht.entrySet()){
             System.out.println(e.getKey()+" "+e.getValue());
