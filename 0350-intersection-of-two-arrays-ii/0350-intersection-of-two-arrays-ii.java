fa5826458b875290ea50a7201b6df3cd9b913e7d
Giriraj <150998984+Giriraj-code@users.noreply.github.com>
2026-09-22 11:18:36 +0530

    Time: 3 ms (93.87%), Space: 45.3 MB (20.53%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,23 +1,32 @@
-
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+
         Map<Integer, Integer> freq = new HashMap<>();
 
-      
         for (int num : nums1) {
             freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
 
-        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+        int[] result = new int[nums1.length];
         int k = 0;
 
         for (int num : nums2) {
-            if (freq.getOrDefault(num, 0) > 0) {
-                temp[k++] = num;
-                freq.put(num, freq.get(num) - 1);
+            int count = freq.getOrDefault(num, 0);
+
+            if (count > 0) {
+                result[k++] = num;
+
+                if (count == 1) {
+                    freq.remove(num);
+                } else {
+                    freq.put(num, count - 1);
+                }
             }
         }
 
-        return Arrays.copyOf(temp, k);
+        return Arrays.copyOf(result, k);
     }
 }
