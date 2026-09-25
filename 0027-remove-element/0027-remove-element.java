c229efe59db6bd02bead24b486aab50338a31c1b
Giriraj <150998984+Giriraj-code@users.noreply.github.com>
2026-09-25 15:09:23 +0530

    Time: 0 ms (100%), Space: 43.4 MB (61.05%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,12 +1,17 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] != val){
-                nums[k] = nums[i];
-                k++;
+        int i = 0;
+        int n = nums.length;
+        
+        while (i < n) {
+            if (nums[i] == val) {
+                // Replace current element with the last element, then shrink the array size
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
             }
         }
-        return k;
+        return n;
     }
 }
